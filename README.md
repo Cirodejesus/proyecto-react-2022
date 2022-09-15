b432c35a480ff7aa5153dcdd9dd9a4d7241245db
Cirodejesus <cjarrieta2507@hotmail.com>
2022-09-15 11:22:39 -0500

    Se creo la lista de criptomonedas, se agregó Axios y se creó el archivo CSS

--- README.md
+++
@@ -1,3 +0,0 @@
-# Proyecto React 2022
-
-Este es un proyecto hecho en React.js que permite conectar a la API provista para acortar URLs
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,45 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+import "./App.css"
+
+
 const App = () => {
+
+  const API_URL = import.meta.env.VITE_API_URL
+
+  const [criptos, setCriptos] = useState()
   
+  useEffect(() => {
+    axios.get(`${API_URL}assets`)
+    .then((data) => {
+      setCriptos(data.data.data)
+
+    })
+   /* fetch(`${API_URL}assets`)
+    .then(response => response.json())
+    .then((data) => {
+      setCriptos(data.data)
+    })*/
+    .catch(() => {
+      console.error("Lapetición falló")
+    })
+  }, [])
+   
+  if(!criptos) return <span>Cargando...</span>
+
+
   return (
-    
-     <h1>Hola Mundo!!!Hola ciro como amanecio hoy papa!</h1>
-  )
+    <>
+     <h1>Lista de criptomonedas</h1>
+    <ol>
+    {
+      criptos.map(({id, name, priceUsd}) => (
+      <li key={id}> Nombre: {name} Precio: {priceUsd}</li>
+    ))
+  }
+    </ol>
+    </>
+    )
 
   }
 export default App
